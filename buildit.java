5d7528e5acd3e46d8a905be3fe88744314366659
Utkarshhrm27 <utkarshrmishra27@gmail.com>
2026-06-04 21:06:00 +0530

    Reverse an array (2-pointer Approach)

--- buildit.java
+++ buildit.java
@@ -1,8 +1,7 @@
-import java.util.concurrent.ForkJoinPool;
-
 public class buildit {
     public static void main(String[] args) {
 
+        // LARGEST ELEMENT IN AN ARRAY
         // BRUTE FORCE
         // int temp;
         // for (int i = 0; i < arr.length ; i++) {
@@ -18,15 +17,32 @@ public class buildit {
         // System.out.println(arr[arr.length-1]);
 
         // OPTIMAL
-        int[] arr = { 3, 2, 1, 5, 2 };
-        int largest = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > largest) {
-                largest = arr[i];
-            } else {
-            }
-        }
-        System.out.println(largest);
+        // int[] arr = { 3, 2, 1, 5, 2 };
+        // int largest = arr[0];
+        // for (int i = 0; i < arr.length; i++) {
+        // if (arr[i] > largest) {
+        // largest = arr[i];
+        // } else {
+        // }
+        // }
+        // System.out.println(largest);
+
+        
+        //REVERSE AN ARRAY 2-POINTER APPROACH
+        // int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        // int temp;
+        // int i;
+        // int j;
+        // for (i = 0, j = arr.length - 1; i < j; i++, j--) {
+        //     temp = arr[i];
+        //     arr[i] = arr[j];
+        //     arr[j] = temp;
+
+        // }
+
+        // for (int k = 0; k < arr.length; k++) {
+        //     System.out.println(arr[k]);
+        // }
 
     }
 
